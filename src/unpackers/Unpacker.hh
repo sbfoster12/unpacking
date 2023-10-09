4a96036e6bf806188385d1971e5df01fab652f9e
Sean Foster <sbfoster@bu.edu>
2023-10-09 10:40:44 -0400

    Change get method to return a vector of the objects instead of pointer

--- src/unpackers/Unpacker.hh
+++ src/unpackers/Unpacker.hh
@@ -26,31 +26,32 @@ namespace unpackers {
         std::map<std::string,std::shared_ptr<dataProducts::DataProductPtrCollection>>
         GetCollections();
 
-        // This template function returns a std::vector<std::shared_ptr<T>>
-        // extracted from the collectionPtrs map. The map stores the vector of
+        // This template function returns a std::vector<T>
+        // extracted from the collectionPtrs map. That map stores the vector of
         // base DataProduct pointers, so we have to individually cast 
-        // each back to its original derived class
+        // each back to its original derived class. Here we return a vector
+        // of the objects (no pointers)
+        // The dynamic cast to T must be valid or else an empty vector is returned
         template <typename T>
-        std::vector<std::shared_ptr<T>> GetCollection(std::string collectionName) {
+        std::vector<T> GetCollection(std::string collectionName) {
             //Get the collection of base pointers
             auto colBasePtr = basePtrCol[collectionName];
-            std::cout << colBasePtr->size() << std::endl;
-            //Create collection of derive pointers
-            std::vector<std::shared_ptr<T>> derivedPtrCol;
-            //Loop over base pointers and cast to derived
+            //Create dervied object (not pointer) collection
+            std::vector<T> derivedCol;
+            //Loop over references to base pointers and cast to derived
             //Exit if casting fails
-            //Return collection of derived pointers
+            //Return collection of derived objects (not pointers)
+            //These derived objects are copies - is this okay?
             for (const auto& basePtr : *colBasePtr) {
-                auto derivedPtr = std::dynamic_pointer_cast<T>(basePtr);
-                if (derivedPtr != nullptr) {
-                    derivedPtrCol.push_back(derivedPtr);
+                if (auto derivedPtr = dynamic_cast<T*>(basePtr.get())) {
+                    derivedCol.push_back(*derivedPtr);
                 } else {
-                    std::cerr << "Collection could not be found with";
-                    std::cout << "the provided template" << std::endl;
-                    exit(1);
+                    std::cerr << "Collection could not be found with the provided template.\n"
+                        << "Returning an empty vector\n";
+                    return {};
                 }
             }
-            return derivedPtrCol;
+            return derivedCol;
         }
 
         void ClearCollections();
